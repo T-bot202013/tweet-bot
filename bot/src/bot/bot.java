ffd4b2c9df93e54ba692aa8d249982f58a55f0eb
T-bot202013 <58537602+T-bot202013@users.noreply.github.com>
2019-12-18 10:11:12 +0900

    Add files via upload

--- bot/src/bot/bot.java
+++ bot/src/bot/bot.java
@@ -17,15 +17,21 @@ import twitter4j.auth.AccessToken;
 		private static String consumerSecret = "jAsrUiXbbx24DcPHFax1oJeCSw53DLzMqcc0lDbOaKBsdoJIhk";
 		private static String accessToken = "1196226134209228800-nd5OLsaSgLNNhMotyODXsASEogcA46";
 		private static String accessTokenSecret = "YREMOxLKU3iCcj3xeA4hyA5hzRzT95Y3M3wgxFG29sVwl";
-		private static String tweet="準備中";
-						 }
+		private String getTweet(){
+			String tweets[] = {
+					"現在準備中",
+					"ネタ不足",
+			};
+			int randint = (int)(Math.random()*tweets.length);
+			return tweets[randint];
+		}
 
 		public void main(HttpServletRequest req, HttpServletResponse resp)
 				throws IOException{
+			String message = getTweet();
 			Twitter twitter = new TwitterFactory().getInstance();
 			twitter.setOAuthConsumer(consumerKey, consumerSecret);
 			twitter.setOAuthAccessToken(new AccessToken(accessToken,accessTokenSecret));
-			String message = gtweet();
 			try{
 				twitter.updateStatus(message);
 				System.out.println("成功");
